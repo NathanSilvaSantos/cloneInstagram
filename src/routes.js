712a0e8851ee6afcefb4981873b98dc33edc73cd
NathanSilvaSantos <nathan013silva@gmail.com>
2020-09-23 13:05:04 -0300

    criando cabeçalho

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Image } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import Feed from './pages/Feed';
+import Logo from './assets/instagram.png';
 
 const Routes = createAppContainer(
     createStackNavigator({
         Feed
+    }, {
+        headerLayoutPreset: 'center',
+        defaultNavigationOptions: {
+            headerTitle: <Image source={Logo} />,
+            headerStyle: {
+                backgroundColor: '#f5f5f5'
+            }
+        },
     })
 );
 
