bca7c5a2525c9786cec420a32e0314b0a795d3be
NathanSilvaSantos <nathan013silva@gmail.com>
2020-09-25 22:41:54 -0300

    criando scroll infinito

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -3,19 +3,26 @@ import { View, FlatList } from 'react-native';
 import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
 export default function Feed() {
     const [ feed, setFeed] = useState([]);
+    const [ page, setPage] = useState(1);
+    const [ total, setTotal] = useState(0);
+
+    async function loadPage(pageNumber = page, shouldRefresh) {
+        if (total && pageNumber > total) return;
+
+        const response = await fetch(
+            `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+        );
+
+        const data = await response.json();
+        const totalItems = response.headers.get('X-Total-Count');
+
+        setTotal(Math.floor(totalItems / 5));
+        setFeed([...feed, ...data]);
+        setPage(pageNumber + 1);
+    }
 
     useEffect(() => {
-        async function loadFeed() {
-            const response = await fetch(
-                'http://localhost:3000/feed?_expand=author&limit=5&_page=1'
-            );
-
-            const data = await response.json();
-
-            setFeed(data);
-        }
-
-        loadFeed();
+        loadPage();
     }, []);
     return (
         <View >
